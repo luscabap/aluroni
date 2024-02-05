22b5a92666cd2ea4c07814c6f819c908f1a08219
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-02-05 20:27:47 -0300

    Tratamento das rotas

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -1,5 +1,7 @@
 import cardapio from "data/cardapio.json";
 import styles from "./Inicio.module.scss";
+import stylesTema from '../../styles/Tema.module.scss';
+import nossaCasa from './nossa_casa.png'
 
 export default function Inicio() {
     let pratosRecomendados = [...cardapio];
@@ -7,7 +9,7 @@ export default function Inicio() {
 
     return(
         <>
-            <h3 className={styles.titulo}>Recomendações da cozinha</h3>
+            <h3 className={stylesTema.titulo}>Recomendações da cozinha</h3>
             <div className={styles.recomendados}>
                 {pratosRecomendados.map(item => (
                     <div key={item.id} className={styles.recomendado}>
@@ -17,6 +19,13 @@ export default function Inicio() {
                         </div>
                     </div>
                 ))}
+            </div>
+            <h3 className={stylesTema.titulo}>Nossa casa</h3>
+            <div className={styles.nossaCasa}>
+                <img src={nossaCasa} alt="Casa do Aluroni" />
+                <div className={styles.nossaCasa__endereco}>
+                    Rua Teste, 3185 <br/><br/> Vila Mariana - SP
+                </div>
             </div>
         </>
     );
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
+import Footer from 'components/Footer';
 import Header from 'components/Header';
 import Menu from 'components/Menu';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
+import NotFound from 'pages/NotFound';
+import Sobre from 'pages/Sobre';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function AppRouter(){
@@ -13,7 +16,10 @@ export default function AppRouter(){
                 <Routes>
                     <Route path="/" element={<Inicio />}/>
                     <Route path="/cardapio" element={<Cardapio />}/>
+                    <Route path="/sobre" element={<Sobre />}/>
+                    <Route path="*" element={<NotFound />}/>
                 </Routes>
+                <Footer />
             </Router>
         </main>
     );
